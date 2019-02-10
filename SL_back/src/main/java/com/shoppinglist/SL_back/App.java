3e7479dd5157865ea3df6148b653a4cfcc3c4d1b
anccelo_Sgt <angelo.la-greca@sogeti.com>
2019-02-10 14:14:47 +0100

    create SL_Back

---
+++ SL_back/src/main/java/com/shoppinglist/SL_back/App.java
@@ -0,0 +1,13 @@
+package com.shoppinglist.SL_back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
